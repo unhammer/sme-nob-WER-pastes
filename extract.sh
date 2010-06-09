4d2d695b9f8823c94b22476594354bb8a26dd521
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-06-09 11:40:38 +0200

    rename

--- extract.sh
+++ extract.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -eq 1 ]; then 
-    git show "$1":current-paste > old.tmp
-    cp current-paste new.tmp
-elif [ "$#" -eq 2 ]; then 
-    git show "$1":current-paste > old.tmp
-    git show "$2":current-paste > new.tmp
+get_and_touch () {
+    git show "$1":current-paste > "$2";
+    D=$(git log --date=iso8601 "$1" -- current-paste | head -n3 | tail -n1 |\
+sed 's/Date: *\(....\)-\(..\)-\(..\) \(..\):\(..\):\(..\).*/\1\2\3\4\5.\6/');
+    touch -m -t "$D" "$2"
+}
+
+if [ "$#" -eq 1 ]; then
+    get_and_touch "$1" old.tmp
+    cp -p current-paste new.tmp
+elif [ "$#" -eq 2 ]; then    
+    get_and_touch "$1" old.tmp
+    get_and_touch "$2" new.tmp
 else
     echo "Usage: sh extract.sh old-commit [new-commit]"
     echo "Arguments are commit names understandable by git. If new-commit is not given, HEAD is used."

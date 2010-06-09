b9cf2ef2e036ec77b8b5e7fe52c9f53dcefca436
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-06-09 11:03:41 +0200

    update-wdiff-with-last.sh, assumes wdiff-to-html.sh is in parent dir

---
+++ update-wdiff-with-last.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    # HEAD^1 might be a change that didn't affect the file current-paste
+    LAST=$(git whatchanged -n 2 --pretty=oneline current-paste | grep -v 'current-paste *$'| cut -d' ' -f1 | tail -n 1)
+    ./extract.sh ${LAST} && ../wdiff-to-html.sh old.tmp new.tmp > wdiff.html
+elif [ "$#" -eq 1 ]; then
+    ./extract.sh "$1" && ../wdiff-to-html.sh old.tmp new.tmp > wdiff.html
+else
+    echo "Usage: sh wdiff-to-html.sh [commit]"; 
+    echo "where, by default, commit is the last one changing current-paste,";
+    echo "otherwise a commit name understandable by git";
+    exit 1; 
+fi

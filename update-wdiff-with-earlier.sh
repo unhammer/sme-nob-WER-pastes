c9e636b1a649e6ee69ea2505ddbd1d3c8110d15d
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-06-15 11:08:06 +0200

    path fix

--- update-wdiff-with-earlier.sh
+++ update-wdiff-with-earlier.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-PASTES=$(dirname $0)
+WER=/l/s/WER
+echo "Path to the WER directory in apertium-sme-nob: ${WER}";
+
+
+PASTES=$(cd $(dirname $0) && pwd)
+
 
 cd ${PASTES}
 
 if [ "$#" -eq 0 ]; then
     # HEAD^1 might be a change that didn't affect the file current-paste
     LAST=$(git whatchanged -n 2 --pretty=oneline current-paste | grep -v 'current-paste *$'| cut -d' ' -f1 | tail -n 1)
-    ./extract.sh ${LAST} && ./wdiff-to-html.sh old.tmp new.tmp > wdiff.html
+    ./extract.sh ${LAST} && ${WER}/wdiff-to-html.sh old.tmp new.tmp > wdiff.html
 elif [ "$#" -eq 1 ]; then
-    ./extract.sh "$1" && ./wdiff-to-html.sh old.tmp new.tmp > wdiff.html
+    ./extract.sh "$1" && ${WER}/wdiff-to-html.sh old.tmp new.tmp > wdiff.html
 else
     echo "Usage: sh wdiff-to-html.sh [commit]"; 
     echo "where, by default, commit is the last one changing current-paste,";

12dd73d1ed47b9810c9cb14379588e1acd5f997f
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-06-15 11:05:19 +0200

    fix

--- make-current-paste.sh
+++ make-current-paste.sh
@@ -3,6 +3,15 @@
 WER=/l/s/WER
 echo "Path to the WER directory in apertium-sme-nob: ${WER}";
 
-PASTES=$(dirname $0)
 
-cd ${WER} && make -j2 && ./make-paste.sh > ${PASTES}/current-paste && cd ${PASTES} && GIT_PAGER='' git diff && echo "Now do:" && echo "sh ${PASTES}/update-wdiff-with-earlier.sh"
+PASTES=$(cd $(dirname $0) && pwd)
+
+
+cd ${WER} 
+make -j2 && sh make-paste.sh > ${PASTES}/current-paste
+if [ $? -ne 0 ]; then
+    cd ${PASTES}
+    GIT_PAGER='' git diff current-paste
+    echo "Now do:"
+    echo "sh ${PASTES}/update-wdiff-with-earlier.sh"
+fi

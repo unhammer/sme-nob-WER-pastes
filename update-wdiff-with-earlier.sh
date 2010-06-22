78079d53ac7347bca23dc52c492d7768cb7df734
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-06-22 12:12:28 +0200

    `git log --first-parent -2` is much prettier

--- update-wdiff-with-earlier.sh
+++ update-wdiff-with-earlier.sh
@@ -11,7 +11,7 @@ cd ${PASTES}
 
 if [ "$#" -eq 0 ]; then
     # HEAD^1 might be a change that didn't affect the file current-paste
-    LAST=$(git whatchanged -n 2 --pretty=oneline current-paste | grep -v 'current-paste *$'| cut -d' ' -f1 | tail -n 1)
+    LAST=$(git log --first-parent -2 --pretty=%H -- current-paste | tail -n1)
     ./extract.sh ${LAST} && ${WER}/wdiff-to-html.sh old.tmp new.tmp > wdiff.html
 elif [ "$#" -eq 1 ]; then
     ./extract.sh "$1" && ${WER}/wdiff-to-html.sh old.tmp new.tmp > wdiff.html
